39bb0747890f5127b01189be07d7315e97d297e4
Zifan Xu <zx712@nyu.edu>
2019-07-24 18:26:25 -0400

    update crontab

--- src/crontab/scheduleCron.py
+++ src/crontab/scheduleCron.py
@@ -1,7 +1,9 @@
 from crontab import CronTab
  
 my_cron = CronTab(user='xuzifan')
-job = my_cron.new(command='python3 automatic_ETL.py')
+job = my_cron.new(command='python3 data_cleaning.py')
 job.minute.every(1)
+
+
  
 my_cron.write()
